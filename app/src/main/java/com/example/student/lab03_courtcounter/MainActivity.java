b013ec3cd11d72982958b30fa73812576cdf068b
R7232 <iver0605@gmail.com>
2016-03-15 10:55:21 +0800

    初始

--- app/src/main/java/com/example/student/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/lab03_courtcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.lab03_courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int mFraction ;
+
+
+    public void fraction3(View view) {
+        mFraction += 3 ;
+        TextView fraction = (TextView)findViewById(R.id.fraction) ;
+        fraction.setText(String.valueOf(mFraction));
+    }
+
+    public void fraction2(View view) {
+        mFraction += 2 ;
+        TextView fraction = (TextView)findViewById(R.id.fraction) ;
+        fraction.setText(String.valueOf(mFraction));
+    }
+
+    public void penalty(View view) {
+        mFraction += 1 ;
+        TextView fraction = (TextView)findViewById(R.id.fraction) ;
+        fraction.setText(String.valueOf(mFraction));
+    }
+    public void reset(View view) {
+        mFraction = 0 ;
+        TextView fraction = (TextView)findViewById(R.id.fraction) ;
+        fraction.setText(String.valueOf(mFraction));
+    }
+
 }
